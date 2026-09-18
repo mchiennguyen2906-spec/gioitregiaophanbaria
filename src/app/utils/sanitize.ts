ce97d602175bbcb44f4546d268dcddc4536a8934
hiennhxai <mc.hiennguyen2906@gmail.com>
2026-09-18 19:28:18 +0700

    fix(ui): allow advanced img attributes in sanitize-html to prevent images from disappearing from article content

--- src/app/utils/sanitize.ts
+++ src/app/utils/sanitize.ts
@@ -5,12 +5,13 @@ export const sanitize = (html: string) => {
 
 
   return sanitizeHtml(preprocessedHtml, {
-    allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'img', 'iframe' ]),
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'img', 'iframe', 'figure', 'figcaption', 'picture', 'source' ]),
     allowedAttributes: {
       ...sanitizeHtml.defaults.allowedAttributes,
       '*': ['style', 'class'],
       'iframe': ['src', 'width', 'height', 'allow', 'allowfullscreen', 'frameborder'],
-      'img': ['src', 'alt', 'width', 'height']
+      'img': ['src', 'alt', 'width', 'height', 'srcset', 'sizes', 'loading', 'data-src', 'data-lazy-src'],
+      'source': ['src', 'srcset', 'media', 'type']
     },
     allowedIframeHostnames: ['www.youtube.com', 'youtube.com', 'youtu.be', 'player.vimeo.com']
   });
